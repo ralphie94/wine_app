a2ca548e46a48f415ae3ab4244a5321492e62cc3
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-05-24 15:49:37 -0700

    add models and blueprint to app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, g
+import models
+from resources.users import users_api
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+app.register_blueprint(users_api, url_prefix='/users')
 
 @app.before_request
+def before_request():
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 @app.route('/')
-def index():
+def hello():
     return 'hi'
 
-if __name__ == 'main':
+if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
