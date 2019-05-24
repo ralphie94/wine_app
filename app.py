5165926cedd78d3ebbb49fe5df5b8529dc85f1fc
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-05-24 16:13:35 -0700

    add CORS and login manager

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from flask import Flask, g
 import models
 from resources.users import users_api
 
-DEBUG = True
-PORT = 8000
+from flask_cors import CORS
+from flask_login import LoginManager
+login_manager - LoginManager()
+
+import config
 
 app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+CORS(users_api, origins=["http://localhost:3000"], supports_credentials=True)
 app.register_blueprint(users_api, url_prefix='/users')
 
 @app.before_request
