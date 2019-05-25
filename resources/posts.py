ede40b10e65703a6b824d97e5d5ae11bf333bf9a
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-05-25 12:54:34 -0700

    Create Post get by id and edit route

--- resources/posts.py
+++ resources/posts.py
@@ -50,10 +50,63 @@ class PostList(Resource):
         post = models.Post.create(**args)
         return post
 
+
+class Post(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'posted_by',
+            required=False,
+            help='username is required to post',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'name',
+            required=False,
+            help='Name of wine is required',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'vintage',
+            required=False,
+            help='No vintage year was provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'review',
+            required=False,
+            help='No review/comment',
+            location=['form', 'json']
+        )
+        super().__init__()
+
+    @marshal_with(post_fields)
+    def get(self, id):
+        try:
+            post = (models.Post.get(models.Post.id==id), 200)
+        except models.Post.DoesNotExist:
+            abort(404)
+        else:
+            return post
+
+    @marshal_with(post_fields)
+    def put(self, id):
+        args = self.reqparse.parse_args()
+        query = models.Post.update(**args).where(models.Post.id==id)
+        query.execute()
+        return (models.Post.get(models.Post.id==id), 200)
+
+
+
 posts_api = Blueprint('resources.posts', __name__)
 api = Api(posts_api)
 api.add_resource(
     PostList,
     '/posts',
     endpoint='posts'
+)
+api.add_resource(
+    Post,
+    '/posts/<int:id>',
+    endpoint='post'
 )
