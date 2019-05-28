ef41340a71c9abc1358307702943989ec71695f4
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-05-28 11:27:44 -0700

    winesqlite

--- resources/users.py
+++ resources/users.py
@@ -6,6 +6,8 @@ from flask_restful import (Resource, Api, reqparse, inputs, fields, marshal, mar
 
 from flask_login import login_user, logout_user, login_required, current_user
 
+from flask_bcrypt import check_password_hash
+
 import models
 
 user_fields = {
@@ -47,9 +49,58 @@ class UserList(Resource):
                 'error': 'Password and password verification do not match'
             }), 400) 
 
+
+class User(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'username',
+            required=True,
+            help='No username provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required=True,
+            help='No password provided',
+            location=['form', 'json']
+        )
+        super().__init__()    
+    
+    def post(self):
+        try:
+            args = self.reqparse.parse_args()
+            user = models.User.get(models.User.username==args['username'])
+            if(user):
+                if(check_password_hash(user.password, args['password'])):
+                    return make_response(
+                        json.dumps({
+                            'user': marshal(user, user_fields),
+                            'message':"success"
+                        }), 200)
+                else:
+                    return make_response(
+                        json.dumps({
+                            'message':"incorrect password"
+                        }), 200)
+        except models.User.DoesNotExist:
+            return make_response(
+                    json.dumps({
+                        'message':"Username does not exist"
+                    }), 200)
+        
+
+
+
+
 users_api = Blueprint('resources.users', __name__)
 api = Api(users_api)
 api.add_resource(
     UserList,
     '/registration'
+)
+
+api.add_resource(
+    User,
+    '/login'
 )
